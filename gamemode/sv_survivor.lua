61f8ca702791a145b57cbace37de265a0ce6220a
SockRocks <vulturebabynugget@gmail.com>
2025-05-19 23:07:02 -0400

    fixed stamina system to exclude zombie king

--- gamemode/sv_survivor.lua
+++ gamemode/sv_survivor.lua
@@ -1,20 +1,24 @@
 local sprintTable = {} -- monitors the stamina of all players
+local BASE_STAMINA = 100
 
 function manageStamina()
     for k, v in ipairs(player.GetAll()) do
-        if not sprintTable[v] then
-            sprintTable[v] = {stamina = 100, baseRun = v:GetRunSpeed()}
-        end
-        if v:KeyDown(IN_SPEED) then
-            if sprintTable[v].stamina <= 0 then
-                v:SetRunSpeed(v:GetWalkSpeed())
-            else
-                sprintTable[v].stamina = sprintTable[v].stamina - 0.5
-                v:SetRunSpeed(sprintTable[v].baseRun)
+        if v ~= GAMEMODE.ZOMBIE_KING then
+            if not sprintTable[v] then
+                sprintTable[v] = {stamina = BASE_STAMINA, baseRun = v:GetRunSpeed()}
             end
-        else
-            if sprintTable[v].stamina < 100 then
-                sprintTable[v].stamina = sprintTable[v].stamina + 0.5
+
+            if v:KeyDown(IN_SPEED) then
+                if sprintTable[v].stamina <= 0 then
+                    v:SetRunSpeed(v:GetWalkSpeed())
+                else
+                    sprintTable[v].stamina = sprintTable[v].stamina - 0.5
+                    v:SetRunSpeed(sprintTable[v].baseRun)
+                end
+            else
+                if sprintTable[v].stamina < BASE_STAMINA then
+                    sprintTable[v].stamina = sprintTable[v].stamina + 0.5
+                end
             end
         end
     end
